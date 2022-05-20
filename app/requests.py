8441ee73d785c24f16ff0672241bc715bf811d2f
gwashh <perrysugut75@gmail.com>
2022-05-20 11:32:02 +0300

    modified files

--- app/requests.py
+++ app/requests.py
@@ -9,7 +9,7 @@ def configure_request(app):
 
 def get_quote():
     get_quote_url = base_url
-    
+     
     with urllib.request.urlopen(get_quote_url) as url:
         get_quote_data = url.read()
         get_quote_response = json.loads(get_quote_data)
